f2b07c8a2f17dbe802a720e3654dd3655c8348fa
Jaedon Naidu <jaedon.naidu@gmail.com>
2024-04-05 00:10:32 +0200

    changed username to spy name. Also added some error messages for spy name, eg TOO SHORT or TAKEN

--- Project1/Project1/Login.cpp
+++ Project1/Project1/Login.cpp
@@ -58,7 +58,7 @@ void Login::createLoginScreen(const char* textureSheet, SDL_Renderer* renderer)
 	checkActive = true;
 
 	// Create RenderText object for username display
-	usernameText = new RenderText(100, 300, 110, renderer, "Username:  ", {0 ,0 ,0});
+	usernameText = new RenderText(100, 300, 110, renderer, "Spy name:  ", {0 ,0 ,0});
 	userInputText = new RenderText(600, 300, 110, renderer, userInput.c_str(), {0, 0, 0});
 
 	SDL_StartTextInput();
@@ -70,9 +70,9 @@ void Login::update()
 {
 	// Get the state of the keyboard
 	const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-	
+
 	// Ensures that the user can enter at most 10 characters for their name
-	if (userInput.length() < 10) 
+	if (userInput.length() < 10)
 	{
 		while (SDL_PollEvent(&event)) {
 			switch (event.type)
@@ -87,16 +87,25 @@ void Login::update()
 			}
 		}
 	}
-	
+
 	// If the user presses backspace, it clears their entire entry
 	//Edit: @jaedonnaidu: it's now able to backspace one character at a time
 	if (currentKeyStates[SDL_SCANCODE_BACKSPACE] != 0)
 	{
 		//don't allow to backspace if string is empty
-		if(userInput.length() > 0)
+		if (userInput.length() > 0)
 		{
-			//erase a character
-			userInput.erase(userInput.length() - 1, 1);
+			//A backspace should erase the whole thing if the thing is a message we produced
+			if(userInput == "TOO SHORT" || userInput=="TAKEN")
+			{
+				userInput.erase(0, userInput.length());
+
+			//Otherwise a backspace should just erase a character
+			}else
+			{
+				userInput.erase(userInput.length() - 1, 1);
+			}
+			
 
 			//update the text
 			userInputText->updateText(this->renderer, userInput);
@@ -104,50 +113,56 @@ void Login::update()
 			//use a small wait because a standard backspace key press takes around 200ms, so we wouldn't want one press to continue deleting characters
 			this_thread::sleep_for(chrono::milliseconds(200));
 		}
-		
+
 	}
 
 	// Press enter to continue to game if the username enetered is of the appropriate length
-	if (currentKeyStates[SDL_SCANCODE_RETURN] != 0
-		&& userInput.length() >= 3
-		&& userInput.length() <= 10)
+	if (currentKeyStates[SDL_SCANCODE_RETURN] != 0)
 	{
+		if (userInput.length() >= 3 && userInput.length() <= 10) {
+			if (usernameExists(userInput))
+			{
+				userInput = "TAKEN";
+				userInputText->updateText(this->renderer, userInput);
+				this_thread::sleep_for(chrono::milliseconds(200));
+			}
+			else if ((userInput != "TAKEN") && (userInput != "TOO SHORT"))
+			{
+				// Used to open/create a text file in overwrite mode. Use ios::app to append.
+				ofstream file("textfiles/PlayerInfo.txt", ios::trunc);
 
-		if(usernameExists(userInput))
+				// Adds username to a user login info textfile found at "textfiles/LoginInfo.txt"
+				string outputString = userInput + "\n";
+				file << outputString;
+
+				file.close();
+
+				// Sets the login screen to false;
+				Login::checkActive = false;
+			}
+		}
+		else if (userInput.length() < 3)
 		{
-			userInput = "TAKEN";
+			userInput = "TOO SHORT";
 			userInputText->updateText(this->renderer, userInput);
 			this_thread::sleep_for(chrono::milliseconds(200));
-		}else if(userInput != "TAKEN")
-		{
-			// Used to open/create a text file in overwrite mode, use "std::ios::app" in the last parameter to append a text file
-			ofstream file("textfiles/LoginInfo.txt", ios::app);
-
-			// Adds username to a user login info textfile found at "textfiles/LoginInfo.txt"
-			string outputString = userInput + "\n";
-			file << outputString;
-
-			file.close();
-
-			// Sets the login screen to false;
-			Login::checkActive = false;
 		}
-		
+
+
 	}
 
-	
 }
 
 //edit: @jaedonnaidu: just checks if username already exists on system
 bool Login::usernameExists(string s){
-	ifstream file("textfiles/LoginInfo.txt");
+	ifstream file("textfiles/Leaderboard.txt");
 	string line;
 
 	if (file.is_open())
 	{
 		while (getline(file, line))
 		{
-			if (line == s)
+			if (line.find(s) != string::npos)
 			{
 				return true;
 			}
